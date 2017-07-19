bad4f0b310dc6ecbcd2cdbc7984684c67b2914bb
tuannt39 <tuannt39@gmail.com>
2017-07-19 13:57:38 +0700

    start project

---
+++ tour-online/src/main/java/vn/hkd/touronline/TourOnlineApplication.java
@@ -0,0 +1,12 @@
+package vn.hkd.touronline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourOnlineApplication.class, args);
+	}
+}
---
+++ tour-online/src/main/resources/toursproject.sql
@@ -0,0 +1,222 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th7 13, 2017 lúc 04:22 AM
+-- Phiên bản máy phục vụ: 10.1.22-MariaDB
+-- Phiên bản PHP: 7.1.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `toursproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `book`
+--
+
+CREATE TABLE `book` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `tour_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date` date DEFAULT NULL,
+  `no_adults` int(11) NOT NULL,
+  `no_>12` int(11) DEFAULT NULL,
+  `no_5-12` int(11) DEFAULT NULL,
+  `no_2-5` int(11) DEFAULT NULL,
+  `no_<2` int(11) DEFAULT NULL,
+  `approved` tinyint(1) DEFAULT NULL,
+  `note` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `customer_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dob` date NOT NULL,
+  `phone_no` int(20) NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `tour_id` int(11) NOT NULL,
+  `script` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `prices`
+--
+
+CREATE TABLE `prices` (
+  `id` int(11) NOT NULL,
+  `tour_id` int(11) NOT NULL,
+  `adult` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `5-12` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `2-5` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `<2` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tours`
+--
+
+CREATE TABLE `tours` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `length` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `origin` varchar(50) CHARACTER SET utf32 NOT NULL,
+  `descript` text CHARACTER SET utf8 NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `addition_service` int(11) DEFAULT NULL,
+  `approved` tinyint(1) NOT NULL,
+  `promotion` text COLLATE utf8mb4_unicode_ci,
+  `price_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tour_guide`
+--
+
+CREATE TABLE `tour_guide` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `script` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `role` tinyint(1) NOT NULL,
+  `username` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pwd` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_no` int(12) NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `book_fk1` (`tour_id`),
+  ADD KEY `book_fk2` (`user_id`);
+
+--
+-- Chỉ mục cho bảng `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `image_fk` (`tour_id`);
+
+--
+-- Chỉ mục cho bảng `prices`
+--
+ALTER TABLE `prices`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `prices_fk` (`tour_id`);
+
+--
+-- Chỉ mục cho bảng `tours`
+--
+ALTER TABLE `tours`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `tour_guide`
+--
+ALTER TABLE `tour_guide`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `book`
+--
+ALTER TABLE `book`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT cho bảng `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT cho bảng `tour_guide`
+--
+ALTER TABLE `tour_guide`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `book`
+--
+ALTER TABLE `book`
+  ADD CONSTRAINT `book_fk1` FOREIGN KEY (`tour_id`) REFERENCES `tours` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `book_fk2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `image_fk` FOREIGN KEY (`tour_id`) REFERENCES `tours` (`id`) ON DELETE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `prices`
+--
+ALTER TABLE `prices`
+  ADD CONSTRAINT `prices_fk` FOREIGN KEY (`tour_id`) REFERENCES `tours` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
