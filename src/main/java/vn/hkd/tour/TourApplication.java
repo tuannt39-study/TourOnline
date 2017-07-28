0f4b4de5e3ee9277309e5f362f14d5f84da8ebf4
Nguyễn Thế Tuân <tuannt39@gmail.com>
2017-07-28 07:27:54 +0700

    khởi tạo project

---
+++ src/main/java/vn/hkd/tour/TourApplication.java
@@ -0,0 +1,12 @@
+package vn.hkd.tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourApplication.class, args);
+	}
+}
